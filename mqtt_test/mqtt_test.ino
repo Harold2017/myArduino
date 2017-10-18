7ce470fafcced72e899f422fe052990f71193526
Harold <pxduharold@126.com>
2017-10-18 16:37:15 +0800

    try2

--- mqtt_test/mqtt_test.ino
+++ mqtt_test/mqtt_test.ino
@@ -4,16 +4,16 @@ const int stepsPerRevolution =4000;
 int steps = 4000/2.5*4*2;
 int buff = 0;
 
-Stepper myStepper_1(stepsPerRevolution, 6, 10);//pin 6 connect to motor 1, pin 9 connect to motor 2
-Stepper myStepper_2(stepsPerRevolution, 11, 9);
+Stepper myStepper(stepsPerRevolution, 6, 9);//pin 6 connect to motor 1, pin 9 connect to motor 2
 
 void setup() {
-  myStepper_1.setSpeed(100);
-  myStepper_2.setSpeed(100);
+  myStepper.setSpeed(100);
   Serial.begin(9600);
 
   pinMode(5,OUTPUT);//Direction pin of motor 1
   pinMode(8, OUTPUT);//Direction pin of motor 2
+  pinMode(4, OUTPUT);//Enable pin of motor 1
+  pinMode(7, OUTPUT);//Enable pin of motor 2
 }
 
 void loop() {
@@ -30,59 +30,73 @@ void loop() {
      
     }
     if (serialData == buff){
-        myStepper_1.step(0);
-        myStepper_2.step(0);
+        myStepper.step(0);
         //Serial.println("IsZero");
     }
     else if(serialData==0){
-        myStepper_1.step(0);
-        myStepper_2.step(0);
+        myStepper.step(0);
         //Serial.println("IsZero");
     }else if(serialData==1){
+        digitalWrite(4, HIGH);
         digitalWrite(5, LOW);
+        digitalWrite(7, LOW);
       
-        myStepper_1.step(steps);
+        myStepper.step(steps);
         Serial.println("LeftOne");
     }else if(serialData==2){
+        digitalWrite(4, HIGH);
         digitalWrite(5, LOW);
+        digitalWrite(7, LOW);
       
-        myStepper_1.step(steps * 2);
+        myStepper.step(steps * 2);
         Serial.println("LeftTwo");
     }else if(serialData==3){
+        digitalWrite(4, HIGH);
         digitalWrite(5, LOW);
+        digitalWrite(7, LOW);
       
-        myStepper_1.step(steps * 3);
+        myStepper.step(steps * 3);
         Serial.println("LeftThree");
     }else if(serialData==4){
+        digitalWrite(4, HIGH);
         digitalWrite(5, LOW);
+        digitalWrite(7, LOW);
       
-        myStepper_1.step(steps * 4);
+        myStepper.step(steps * 4);
         Serial.println("LeftFour");
     }else if(serialData==5){
+        digitalWrite(7, HIGH);
         digitalWrite(8, HIGH);
+        digitalWrite(4, LOW);
       
-        myStepper_2.step(steps);
+        myStepper.step(steps);
         Serial.println("RightOne");
     }else if(serialData==6){
+        digitalWrite(7, HIGH);
         digitalWrite(8, HIGH);
+        digitalWrite(4, LOW);
       
-        myStepper_2.step(steps * 2);
+        myStepper.step(steps * 2);
         Serial.println("RightTwo");
     }else if(serialData==7){
+        digitalWrite(7, HIGH);
         digitalWrite(8, HIGH);
+        digitalWrite(4, LOW);
       
-        myStepper_2.step(steps * 3);
+        myStepper.step(steps * 3);
         Serial.println("RightThree");
     }else if(serialData==8){
+        digitalWrite(7, HIGH);
         digitalWrite(8, HIGH);
+        digitalWrite(4, LOW);
       
-        myStepper_2.step(steps * 4);
+        myStepper.step(steps * 4);
         Serial.println("RightFour");
     }else{
         //Serial.println("Else");   
     }
     buff = serialData;
-    delay(1000);
+    delay(500);
 
 
 }
